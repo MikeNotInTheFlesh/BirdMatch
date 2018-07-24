543dad3ba3b693d3f3eee12db3a7037246298422
MikeNotInTheFlesh <mmgrace@gmail.com>
2018-07-24 15:51:49 -0500

    Add files via upload
    Initial upload

---
+++ Card.js
@@ -0,0 +1,30 @@
+
+function Card(x, y, cardWidth, id, song = false) {
+  this.x = x;
+  this.y = y;
+  this.w = cardWidth;
+  this.h = this.w * 3/4;
+  this.visible = false;
+  this.id = id;
+  this.song = song;
+  
+  this.show = function() {
+    if (this.visible) {
+      fill(255, 255, 255, 255);
+      stroke(220);
+      // strokeWeight(this.w / 15);
+	  if (this.song) {
+		  rect(this.x, this.y, this.w, this.h, this.w / 30);
+		  image(musicImage, this.x, this.y, this.w, this.h);
+	  } else {
+		// rect(this.x, this.y, this.w, this.h, this.w / 30);
+		image(pics[this.id], this.x, this.y, this.w, this.h);
+	  }
+    } else {
+      noStroke();
+      fill(0, 100, 255, 255);
+      // rect(this.x, this.y, this.w, this.h, this.w / 30);
+      image(cardBack, this.x, this.y, this.w, this.h);
+    }
+  }
+}
